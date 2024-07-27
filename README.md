4c5840baa44511a698b75f9cba4a5964aaeaf562
Viner Abubakirov <viner96@yandex.ru>
2024-07-27 22:21:01 +0500

    Переделал main.py под argparse

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # midi-changer
+
+### Unix
+Требуется, чтобы были установлены: `cmake`, `ffmpeg`
+Установка
+```bash
+python3 -m venv .venv
+source ./.venv/bin/activate
+pip install -U pip
+pip install -r requirements.txt
+```
+
+Запуск
+```bash
+(.venv) pc$ python main.py -s sample.wav -m example.mid -o example-sample.wav
+```
--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 from io import BytesIO
+from argparse import ArgumentParser
 
 from mido import MidiFile
 from src import MidiChanger, AudioTransform
 
 
-with open('audio.wav', 'rb') as f:
-    sample = BytesIO(f.read())
+parser = ArgumentParser(
+    prog="MidiChanger",
+    description="Скрипт для изменение звука у миди файла",
+    epilog="Внизу описаны входные аргументы для запуска скрипта"
+)
 
-midi = MidiFile('SMASH MOUTH.All star.mid')
-transform = AudioTransform(sample)
-changer = MidiChanger(midi, transform)
+parser.add_argument('-s', '--sample', required=True, help="Семпл который будет использоваться как основа (Внимение wav файл)")
+parser.add_argument('-m', '--midi', required=True, help="Миди файл у который будет изменен звучание")
+parser.add_argument('-o', '--output', required=True, help="Название выходного файла")
 
-audio = changer.create()
-file = BytesIO()
-audio.export(file, format='wav')
-file.seek(0)
-with open('test1234.wav', 'wb') as f:
-    f.write(file.read())
+if __name__ == "__main__":
+    args = parser.parse_args()
+    with open(args.sample, 'rb') as f:
+        sample = BytesIO(f.read())
+
+    midi = MidiFile(args.midi)
+    transform = AudioTransform(sample)
+    changer = MidiChanger(midi, transform)
+
+    audio = changer.create()
+    file = BytesIO()
+    audio_format = args.output.split('.')[-1]
+    audio.export(file, format=audio_format)
+    file.seek(0)
+    with open(args.output, 'wb') as f:
+        f.write(file.read())
 
